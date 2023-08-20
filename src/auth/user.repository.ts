c07947da5401d4ac01cf045b297b29cfa4d825df
ik <adventurer-ik@daum.net>
2023-08-20 22:01:14 +0900

    bcrypt 도입 - 비밀 번호 암호화 (salt 방식)

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,20 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCrentialsDto } from './dto/auth-credential.dto';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCrentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    // bcrypt 테스트
+    const salt = await bcrypt.genSalt();
+    console.log('salt', salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log('hashedPassword', hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
